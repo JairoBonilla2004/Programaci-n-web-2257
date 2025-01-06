8c0a170fa3ec645a5c3fa78738b8b0154fe6042b
jsbonilla2 <JairoBonilla2004@users.noreply.github.com>
2025-01-06 10:39:51 -0500

    ejercicio-2

--- 2)Parcial/Ejercicio_2/ejercicio_2.js
+++ 2)Parcial/Ejercicio_2/ejercicio_2.js
@@ -1,4 +1,3 @@
-// 1. Crear una función declarada llamada calcularPromedio
 function calcularPromedio(a, b, c) {
     if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
       throw new Error("Todos los parámetros deben ser números.");
@@ -6,7 +5,6 @@ function calcularPromedio(a, b, c) {
     return (a + b + c) / 3;
   }
   
-  // 2. Declarar una función expresada llamada determinarMayor
   const determinarMayor = function (a, b) {
     if (typeof a !== "number" || typeof b !== "number") {
       throw new Error("Todos los parámetros deben ser números.");
@@ -14,7 +12,6 @@ function calcularPromedio(a, b, c) {
     return a > b ? a : b;
   };
   
-  // 3. Crear una función flecha llamada esPar
   const esPar = (num) => {
     if (typeof num !== "number") {
       throw new Error("El parámetro debe ser un número.");
@@ -22,10 +19,8 @@ function calcularPromedio(a, b, c) {
     return num % 2 === 0;
   };
   
-  // 4. Función anónima autoejecutable
   (function () {
     try {
-      // Usar las funciones anteriores con valores de ejemplo
       const promedio = calcularPromedio(4, 8, 6);
       console.log(`El promedio es: ${promedio}`);
   
